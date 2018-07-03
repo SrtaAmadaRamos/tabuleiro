bbaaa7d7e0c998b455f7f4c2bcf0cab160079e8b
Amanda <35702715+SrtaAmadaRamos@users.noreply.github.com>
2018-07-03 11:09:31 -0300

    Update functions.c
    funcoes terminadas

--- functions.c
+++ functions.c
@@ -1,3 +1,24 @@
+void inicializarMatriz(char matriz[][COMP]){
+    for(int i = 0; i < COMP; i++){
+        for(int j = 0; j < COMP; j++){
+            matriz[i][j] = ' ';
+        }
+    }    
+    
+    matriz[0][0] = '*';
+}
+
+void imprimiMatriz(char matriz[][COMP]){
+  for(int i = 0; i < COMP; i++){ //colunas
+        printf("|");
+        for(int j = 0; j < COMP; j++){               //barrinha no inicio e
+            printf(" %c ", matriz[i][j]);           //fim de cada linha
+        }
+        printf("|\n");
+    }    
+    printf("\n");
+}
+
 int procuraAst(char matriz[][COMP], int posicao[2]){
 	int i, j;
 	for(i=0 ; i < COMP; i++){
@@ -11,8 +32,54 @@ int procuraAst(char matriz[][COMP], int posicao[2]){
   	}
 }
 
-void paraCimahey(char matriz[][COMP], int posicao[2]){
+void paraCimahey(char matriz[][COMP], int p[2]){
 
-    procuraAst(matriz, posicao);
+    procuraAst(matriz, p);
 
+    if ( p[0] != 0 ){
+        matriz[ p[0] ][ p[1] ] = ' ';
+        p[0]--;
+        matriz[ p[0] ][ p[1] ] = '*';
+    }
+
+}
+
+void paraEsquerdahey(char matriz[][COMP], int p[2]){
+
+    procuraAst(matriz, p);
+
+    if ( p[1] != 0 ){
+        matriz[ p[0] ][ p[1] ] = ' ';
+        p[1]--;
+        matriz[ p[0] ][ p[1] ] = '*';
+    }
+
+}
+
+void paraBaixohey(char matriz[][COMP], int p[2]){
+
+    procuraAst(matriz, p);
+
+    if ( p[0] != 3 ){
+        matriz[ p[0] ][ p[1] ] = ' ';
+        p[0]++;
+        matriz[ p[0] ][ p[1] ] = '*';
+    }
+
+}
+
+void paraDireitahey(char matriz[][COMP], int p[2]){
+
+    procuraAst(matriz, p);
+
+    if ( p[1] != 3 ){
+        matriz[ p[0] ][ p[1] ] = ' ';
+        p[1]++;
+        matriz[ p[0] ][ p[1] ] = '*';
+    }
+
+}
+
+void limparTela(){
+   system("@cls||clear");
 }
